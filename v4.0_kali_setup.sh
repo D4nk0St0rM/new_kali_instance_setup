b79461db827da1df199f7f868ee88a69613e235f
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-06-05 11:29:13 +0100

    Update v4.0_kali_setup.sh

--- v4.0_kali_setup.sh
+++ v4.0_kali_setup.sh
@@ -190,3 +190,151 @@ file=~/.config/terminator/config; [ -e "${file}" ] && cp -n $file{,.bkup}
 EOF
 
 
+##### Ivim - all users
+echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}vim${RESET} ~ CLI text editor"
+apt-get -y -qq install vim || echo -e ' '${RED}'[!] Issue with apt-get'${RESET} 1>&2
+#--- Configure vim
+file=/etc/vim/vimrc; [ -e "${file}" ] && cp -n $file{,.bkup}   #~/.vimrc
+([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
+sed -i 's/.*syntax on/syntax on/' "${file}"
+sed -i 's/.*set background=dark/set background=dark/' "${file}"
+sed -i 's/.*set showcmd/set showcmd/' "${file}"
+sed -i 's/.*set showmatch/set showmatch/' "${file}"
+sed -i 's/.*set ignorecase/set ignorecase/' "${file}"
+sed -i 's/.*set smartcase/set smartcase/' "${file}"
+sed -i 's/.*set incsearch/set incsearch/' "${file}"
+sed -i 's/.*set autowrite/set autowrite/' "${file}"
+sed -i 's/.*set hidden/set hidden/' "${file}"
+sed -i 's/.*set mouse=.*/"set mouse=a/' "${file}"
+grep -q '^set number' "${file}" 2>/dev/null || echo 'set number' >> "${file}"                                                                        # Add line numbers
+grep -q '^set autoindent' "${file}" 2>/dev/null || echo 'set autoindent' >> "${file}"                                                                # Set auto indent
+grep -q '^set expandtab' "${file}" 2>/dev/null || echo -e 'set expandtab\nset smarttab' >> "${file}"                                                 # Set use spaces instead of tabs
+grep -q '^set softtabstop' "${file}" 2>/dev/null || echo -e 'set softtabstop=4\nset shiftwidth=4' >> "${file}"                                       # Set 4 spaces as a 'tab'
+grep -q '^set foldmethod=marker' "${file}" 2>/dev/null || echo 'set foldmethod=marker' >> "${file}"                                                  # Folding
+grep -q '^nnoremap <space> za' "${file}" 2>/dev/null || echo 'nnoremap <space> za' >> "${file}"                                                      # Space toggle folds
+grep -q '^set hlsearch' "${file}" 2>/dev/null || echo 'set hlsearch' >> "${file}"                                                                    # Highlight search results
+grep -q '^set laststatus' "${file}" 2>/dev/null || echo -e 'set laststatus=2\nset statusline=%F%m%r%h%w\ (%{&ff}){%Y}\ [%l,%v][%p%%]' >> "${file}"   # Status bar
+grep -q '^filetype on' "${file}" 2>/dev/null || echo -e 'filetype on\nfiletype plugin on\nsyntax enable\nset grepprg=grep\ -nH\ $*' >> "${file}"     # Syntax highlighting
+grep -q '^set wildmenu' "${file}" 2>/dev/null || echo -e 'set wildmenu\nset wildmode=list:longest,full' >> "${file}"                                 # Tab completion
+grep -q '^set invnumber' "${file}" 2>/dev/null || echo -e ':nmap <F8> :set invnumber<CR>' >> "${file}"                                               # Toggle line numbers
+grep -q '^set pastetoggle=<F9>' "${file}" 2>/dev/null || echo -e 'set pastetoggle=<F9>' >> "${file}"                                                 # Hotkey - turning off auto indent when pasting
+grep -q '^:command Q q' "${file}" 2>/dev/null || echo -e ':command Q q' >> "${file}"                                                                 # Fix stupid typo I always make
+#--- Set as default editor
+export EDITOR="vim"   #update-alternatives --config editor
+file=/etc/bash.bashrc; [ -e "${file}" ] && cp -n $file{,.bkup}
+([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
+grep -q '^EDITOR' "${file}" 2>/dev/null || echo 'EDITOR="vim"' >> "${file}"
+git config --global core.editor "vim"
+
+
+##### Install conky
+echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}conky${RESET} ~ GUI desktop monitor"
+apt-get -y -qq install conky || echo -e ' '${RED}'[!] Issue with apt-get'${RESET} 1>&2
+#--- Configure conky
+file=~/.conkyrc; [ -e "${file}" ] && cp -n $file{,.bkup}
+[ -e "${file}" ] || cat <<EOF > "${file}"
+## Useful: http://forums.opensuse.org/english/get-technical-help-here/how-faq-forums/unreviewed-how-faq/464737-easy-configuring-conky-conkyconf.html
+background yes
+#background no     # Kali rolling?
+font Monospace:size=8:weight=bold
+use_xft yes
+update_interval 2.0
+own_window yes
+own_window_type normal
+own_window_transparent yes
+own_window_class conky-semi
+own_window_argb_visual yes   # GNOME & XFCE yes, KDE no
+#own_window_argb_visual no    # Kali rolling?
+own_window_colour brown
+own_window_hints undecorated,below,sticky,skip_taskbar,skip_pager
+double_buffer yes
+maximum_width 260
+draw_shades yes
+draw_outline no
+draw_borders no
+stippled_borders 3
+#border_margin 9   # Old command
+border_inner_margin 9
+border_width 10
+default_color grey
+alignment bottom_right
+#gap_x 55   # KDE
+#gap_x 0    # GNOME
+gap_x 5
+gap_y 0
+uppercase no
+use_spacer right
+
+TEXT
+
+\${color dodgerblue3}SYSTEM ${hr 2}$color
+\${color white}Host$color: $nodename  ${alignr}${color white}Uptime$color: $uptime
+\${color white}Kernel : ${color}$kernel on $machine
+
+\${color dodgerblue3}CPU ${hr 2}$color
+\${color white}MHz$color: ${freq} ${alignr}${color white}Load$color: ${exec uptime | awk -F "load average: "  '{print $2}'}
+\${color white}Tasks$color: $running_processes/$processes ${alignr}${color white}CPU0$color: ${cpu cpu0}% ${color white}CPU1$color: ${cpu cpu1}%
+\${cpugraph cpu0 25,120 000000 white} ${alignr}${cpugraph cpu1 25,120 000000 white}
+\${color white}${cpubar cpu1 3,120} ${alignr}${color white}${cpubar cpu2 3,120}$color
+
+\${color dodgerblue3}FILESYSTEM ${hr 2}$color
+\${color white}root$color ${fs_free_perc /}% free${alignr}${fs_free /}/ ${fs_size /}
+\${fs_bar 3 /}$color
+\${color dodgerblue3}PROCESSES  ${hr 2} $color
+\${color white} NAME             PID     CPU     MEM
+\${color white} ${top name 1}  ${top pid 1}   ${top cpu 1}   ${top mem 1} $color
+ \${top name 2}  ${top pid 2}   ${top cpu 2}   ${top mem 2}
+ \${top name 3}  ${top pid 3}   ${top cpu 3}   ${top mem 3}
+ \${top name 4}  ${top pid 4}   ${top cpu 4}   ${top mem 4}
+ \${top name 5}  ${top pid 5}   ${top cpu 5}   ${top mem 5}
+
+\${color dodgerblue3}LAN eth0 (${addr eth0}) ${hr 2}$color
+\${color white}Down$color:  ${downspeed eth0} KB/s${alignr}${color white}Up$color: ${upspeed eth0} KB/s
+\${color white}Downloaded$color: ${totaldown eth0} ${alignr}${color white}Uploaded$color: ${totalup eth0}
+\${downspeedgraph eth0 25,120 000000 00ff00} ${alignr}${upspeedgraph eth0 25,120 000000 ff0000}$color
+
+\${color dodgerblue3}Wi-Fi (${addr wlan0}) ${hr 2}$color
+\${color white}Down$color:  ${downspeed wlan0} KB/s${alignr}${color white}Up$color: ${upspeed wlan0} KB/s
+\${color white}Downloaded$color: ${totaldown wlan0} ${alignr}${color white}Uploaded$color: ${totalup wlan0}
+\${downspeedgraph wlan0 25,120 000000 00ff00} ${alignr}${upspeedgraph wlan0 25,120 000000 ff0000}$color
+
+\${color dodgerblue3}CONNECTIONS ${hr 2}$color
+\${color white}Inbound: $color${tcp_portmon 1 32767 count}  ${alignc}${color white}Outbound: $color${tcp_portmon 32768 61000 count}${alignr}${color white}Total: $color${tcp_portmon 1 65535 count}
+\${color white}Inbound ${alignr}Local Service/Port$color
+\$color ${tcp_portmon 1 32767 rhost 0} ${alignr}${tcp_portmon 1 32767 lservice 0}
+\$color ${tcp_portmon 1 32767 rhost 1} ${alignr}${tcp_portmon 1 32767 lservice 1}
+\$color ${tcp_portmon 1 32767 rhost 2} ${alignr}${tcp_portmon 1 32767 lservice 2}
+\${color white}Outbound ${alignr}Remote Service/Port$color
+\$color ${tcp_portmon 32768 61000 rhost 0} ${alignr}${tcp_portmon 32768 61000 rservice 0}
+\$color ${tcp_portmon 32768 61000 rhost 1} ${alignr}${tcp_portmon 32768 61000 rservice 1}
+\$color ${tcp_portmon 32768 61000 rhost 2} ${alignr}${tcp_portmon 32768 61000 rservice 2}
+
+\${color dodgerblue3}MEMORY & SWAP \${hr 2}\$color
+\${color white}RAM $color  $alignr $memperc%   ${membar 6,170} $color
+\${color white}Swap $color   $alignr $swapperc%   ${swapbar 6,170} $color
+
+EOF
+#--- Add to startup (each login)
+file=/usr/local/bin/start-conky; [ -e "${file}" ] && cp -n $file{,.bkup}
+cat <<EOF > "${file}" || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
+#!/bin/bash
+
+[[ -z ${DISPLAY} ]] && export DISPLAY=:0.0
+$(which timeout) 10 $(which killall) -q conky
+$(which sleep) 15s
+$(which conky) &
+EOF
+
+chmod -f 0500 "${file}"
+mkdir -p ~/.config/autostart/
+file=~/.config/autostart/conkyscript.desktop; [ -e "${file}" ] && cp -n $file{,.bkup}
+cat <<EOF > "${file}" || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
+[Desktop Entry]
+Name=conky
+Exec=/usr/local/bin/start-conky
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Type=Application
+Comment=
+EOF
